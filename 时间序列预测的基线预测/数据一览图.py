536e0a5905d50f25d1807b30b73dbf2f627bc124
yangwohenmai <zongxianyang@outlook.com>
2018-12-25 00:23:12 +0800

    update

---
+++ 时间序列预测的基线预测/数据一览图.py
@@ -0,0 +1,10 @@
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+
+series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series.plot()
+pyplot.show()
---
+++ 时间序列预测的基线预测/时间序列预测的基线预测.py
@@ -0,0 +1,41 @@
+from pandas import read_csv
+from pandas import datetime
+from pandas import DataFrame
+from pandas import concat
+from matplotlib import pyplot
+from sklearn.metrics import mean_squared_error
+
+def parser(x):
+	return datetime.strptime('190'+x, '%Y-%m')
+
+series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+# 构建以恶监督学习结构，滞后数据表
+values = DataFrame(series.values)
+dataframe = concat([values.shift(1), values], axis=1)
+dataframe.columns = ['t-1', 't+1']
+print(dataframe.head(5))
+
+# 将数据分割为训练集和测试集
+X = dataframe.values
+train_size = int(len(X) * 0.66)
+train, test = X[1:train_size], X[train_size:]
+train_X, train_y = train[:,0], train[:,1]
+test_X, test_y = test[:,0], test[:,1]
+
+# 持久性模型，这里的映射模型是简单的对应关系
+def model_persistence(x):
+	return x
+
+# 评估
+predictions = list()
+for x in test_X:
+	yhat = model_persistence(x)
+	predictions.append(yhat)
+test_score = mean_squared_error(test_y, predictions)
+print('Test MSE: %.3f' % test_score)
+
+# 画出结果
+pyplot.plot(train_y)
+pyplot.plot([None for i in train_y] + [x for x in test_y])
+pyplot.plot([None for i in train_y] + [x for x in predictions])
+pyplot.show()
