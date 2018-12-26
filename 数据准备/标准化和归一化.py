353270d7691bacbb8d1beaff69b549d4d2da6c76
yangwohenmai <zongxianyang@outlook.com>
2018-12-26 23:44:45 +0800

    update

---
+++ 数据准备/标准化和归一化.py
@@ -0,0 +1,21 @@
+# Normalize time series data
+from pandas import Series
+from sklearn.preprocessing import MinMaxScaler
+# load the dataset and print the first 5 rows
+series = Series.from_csv('daily-minimum-temperatures-in-me.csv', header=0)
+print(series.head())
+# prepare data for normalization
+values = series.values
+values = values.reshape((len(values), 1))
+# train the normalization
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler = scaler.fit(values)
+print('Min: %f, Max: %f' % (scaler.data_min_, scaler.data_max_))
+# normalize the dataset and print the first 5 rows
+normalized = scaler.transform(values)
+for i in range(5):
+	print(normalized[i])
+# inverse transform and print the first 5 rows
+inversed = scaler.inverse_transform(normalized)
+for i in range(5):
+	print(inversed[i])
